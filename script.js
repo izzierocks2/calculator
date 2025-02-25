8b910ce4c2d0fb589c0f71949aec66e03d7f6709
Grace <gracek2705@tuckahoeschools.org>
2025-02-25 13:30:24 +0000

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const additionButton = document.getElementById('addition-button');
+const subtractionButton = document.getElementById('subtraction-button');
+const multiplicationButton = document.getElementById('multiplication-button');
+const divisionButton = document.getElementById('division-button');
