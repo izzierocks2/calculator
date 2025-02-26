2a387e2f93f9ae8dfe7932367539576ab0b44d93
Grace <gracek2705@tuckahoeschools.org>
2025-02-26 13:31:44 +0000

    commit

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ const additionButton = document.getElementById('addition-button');
 const subtractionButton = document.getElementById('subtraction-button');
 const multiplicationButton = document.getElementById('multiplication-button');
 const divisionButton = document.getElementById('division-button');
+const screen = document.querySelector('.screen');
+
+const choices = ["+", "-", "x", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+additionButton.addEventListener("click", () =>{
+    screen.textContent = "+";
+});
+
+/*Allows the "+" to appear larger on my screen*/
+additionButton.addEventListener('click', function() {
+    screen.innerHTML = "<span class='large'>+</span>";
+});
+
+subtractionButton.addEventListener("click", () =>{
+    screen.textContent = "-";
+});
+
+subtractionButton.addEventListener("click", () =>{
+    screen.innerHTML = "<span class='large'>-</span>";
+});
+
+multiplicationButton.addEventListener("click", () =>{
+    screen.textContent = "x";
+});
+
+multiplicationButton.addEventListener("click", () =>{
+    screen.innerHTML = "<span class='large'>x</span>";
+})
