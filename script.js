e13db5afc3a6b2db2af2a3c231ac6b3d19dbd095
Grace <gracek2705@tuckahoeschools.org>
2025-02-28 13:29:00 +0000

    commit

--- script.js
+++ script.js
@@ -12,76 +12,81 @@ const sevenButton = document.getElementById('seven-button');
 const eightButton = document.getElementById('eight-button');
 const nineButton = document.getElementById('nine-button');
 const clearButton = document.getElementById('clear-button');
+const enterButton = document.getElementById('enter-button');
 const screen = document.querySelector('.screen');
+
+let currentNumber = "";
+let previousNumber = "";
+let currentOperation = null;
 
 const choices = ["+", "-", "x", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-/*Allows "+" to appear on screen and appear larger*/
+/*Allows "+" to appear on screen*/
 additionButton.addEventListener("click", () =>{
-    screen.textContent = "+";
-    screen.innerHTML = "<span class='large'>+</span>";
+    screen.textContent += "+";
 });
 
 subtractionButton.addEventListener("click", () =>{
-    screen.textContent = "-";
-    screen.innerHTML = "<span class='large'>-</span>";
+    screen.textContent += "-";
 });
 
 multiplicationButton.addEventListener("click", () =>{
-    screen.textContent = "x";
-    screen.innerHTML = "<span class='large'>x</span>";
+    screen.textContent += "x";
 });
 
 divisionButton.addEventListener("click", () =>{
-    screen.textContent = "/";
-    screen.innerHTML = "<span class='large'>/</span>";
+    screen.textContent += "/";
 });
 
 oneButton.addEventListener("click", () =>{
-    screen.textContent = "1";
-    screen.innerHTML = "<span class='large'>1</span>";
+    screen.textContent += "1";
+    currentNumber += 1;
 });
 
 twoButton.addEventListener("click", () =>{
-    screen.textContent = "2";
-    screen.innerHTML = "<span class='large'>2</span>";
+    screen.textContent += "2";
+    currentNumber += "2";
 });
 
 threeButton.addEventListener("click", () =>{
-    screen.textContent = "3";
-    screen.innerHTML = "<span class='large'>3</span>";
+    screen.textContent += "3";
+    currentNumber += "3";
 });
 
 fourButton.addEventListener("click", () =>{
-    screen.textContent = "4";
-    screen.innerHTML = "<span class='large'>4</span>";
+    screen.textContent += "4";
+    currentNumber += "4";
 });
 
 fiveButton.addEventListener("click", () =>{
-    screen.textContent = "5";
-    screen.innerHTML = "<span class='large'>5</span>";
+    screen.textContent += "5";
+    currentNumber += "5";
 });
 
 sixButton.addEventListener("click", () =>{
-    screen.textContent = "6";
-    screen.innerHTML = "<span class='large'>6</span>";
+    screen.textContent += "6";
+    currentNumber += "6";
 });
 
 sevenButton.addEventListener("click", () =>{
-    screen.textContent = "7";
-    screen.innerHTML = "<span class='large'>7</span>";
+    screen.textContent += "7";
+    currentNumber += "7";
 });
 
 eightButton.addEventListener("click", () =>{
-    screen.textContent = "8";
-    screen.innerHTML = "<span class='large'>8</span>";
+    screen.textContent += "8";
+    currentNumber += "8";
 });
 
 nineButton.addEventListener("click", () =>{
-    screen.textContent = "9";
-    screen.innerHTML = "<span class='large'>9</span>";
+    screen.textContent += "9";
+    currentNumber += "9";
 });
 
 clearButton.addEventListener("click", () =>{
     screen.textContent = " ";
 });
+
+enterButton.addEventListener("click", () =>{
+    
+})
