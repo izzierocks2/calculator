c79066d6749c43096cfa88e8170595df316f17f5
Grace <gracek2705@tuckahoeschools.org>
2025-03-02 20:13:02 +0000

    commit

--- script.js
+++ script.js
@@ -16,11 +16,12 @@ const enterButton = document.getElementById('enter-button');
 const screen = document.querySelector('.screen');
 let currentNumber = "";
 let previousNumber = "";
+let operation = "";
 
 previousNumber = currentNumber;
 currentNumber = "";
 
-const choices = ["+", "-", "x", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const choices = ["+", "-", "x", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Clear", "Enter"];
 
 /*Allows "+" to appear on screen*/
 additionButton.addEventListener("click", () =>{
@@ -44,42 +45,54 @@ divisionButton.addEventListener("click", () =>{
 
 oneButton.addEventListener("click", () =>{
     screen.textContent += "1";
+    currentNumber += "1";
 });
 
 twoButton.addEventListener("click", () =>{
     screen.textContent += "2";
+    currentNumber += "2";
 });
 
 threeButton.addEventListener("click", () =>{
     screen.textContent += "3";
+    currentNumber += "3";
 });
 
 fourButton.addEventListener("click", () =>{
     screen.textContent += "4";
+    currentNumber += "4";
 });
 
 fiveButton.addEventListener("click", () =>{
     screen.textContent += "5";
+    currentNumber += "5";
 });
 
 sixButton.addEventListener("click", () =>{
     screen.textContent += "6";
+    currentNumber += "6";
 });
 
 sevenButton.addEventListener("click", () =>{
     screen.textContent += "7";
+    currentNumber += "7";
 });
 
 eightButton.addEventListener("click", () =>{
     screen.textContent += "8";
+    currentNumber += "8";
 });
 
 nineButton.addEventListener("click", () =>{
     screen.textContent += "9";
+    currentNumber += "9";
 });
 
 clearButton.addEventListener("click", () =>{
     screen.textContent = " ";
+    currentNumber = "";
+    previousNumber = "";
+    operation = "";
 });
 
 let result = 0
