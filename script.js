1b9a49476caefd6590565ae71b687c563293b8ea
Grace <gracek2705@tuckahoeschools.org>
2025-03-02 19:21:26 +0000

    commit

--- script.js
+++ script.js
@@ -14,16 +14,20 @@ const nineButton = document.getElementById('nine-button');
 const clearButton = document.getElementById('clear-button');
 const enterButton = document.getElementById('enter-button');
 const screen = document.querySelector('.screen');
-
 let currentNumber = "";
 let previousNumber = "";
-let currentOperation = null;
+
+previousNumber = currentNumber;
+currentNumber = "";
 
 const choices = ["+", "-", "x", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 /*Allows "+" to appear on screen*/
 additionButton.addEventListener("click", () =>{
+    operation = "+";
     screen.textContent += "+";
+    previousNumber = currentNumber;
+    currentNumber = "";
 });
 
 subtractionButton.addEventListener("click", () =>{
@@ -40,53 +44,47 @@ divisionButton.addEventListener("click", () =>{
 
 oneButton.addEventListener("click", () =>{
     screen.textContent += "1";
-    currentNumber += 1;
 });
 
 twoButton.addEventListener("click", () =>{
     screen.textContent += "2";
-    currentNumber += "2";
 });
 
 threeButton.addEventListener("click", () =>{
     screen.textContent += "3";
-    currentNumber += "3";
 });
 
 fourButton.addEventListener("click", () =>{
     screen.textContent += "4";
-    currentNumber += "4";
 });
 
 fiveButton.addEventListener("click", () =>{
     screen.textContent += "5";
-    currentNumber += "5";
 });
 
 sixButton.addEventListener("click", () =>{
     screen.textContent += "6";
-    currentNumber += "6";
 });
 
 sevenButton.addEventListener("click", () =>{
     screen.textContent += "7";
-    currentNumber += "7";
 });
 
 eightButton.addEventListener("click", () =>{
     screen.textContent += "8";
-    currentNumber += "8";
 });
 
 nineButton.addEventListener("click", () =>{
     screen.textContent += "9";
-    currentNumber += "9";
 });
 
 clearButton.addEventListener("click", () =>{
     screen.textContent = " ";
 });
 
+let result = 0
+
 enterButton.addEventListener("click", () =>{
-    
-})
+    if (operation === "+") {result = Number(previousNumber) + Number(currentNumber)};
+    document.getElementById("display").textContent = result;
+});
